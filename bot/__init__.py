bd8238c7ac47f0c9c59cde7138b61033d04bd467
susi1403 <92388675+susi1403@users.noreply.github.com>
2021-10-12 22:18:42 +0200

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -22,7 +22,7 @@ try:
   api_hash = environ["API_HASH"]
   bot_token = environ.get("BOT_TOKEN", None)
   tg_session = environ.get("TELEGRAM_SESSION", None)
-  sudo_users = list(set(x for x in environ.get("SUDO_USERS", "999197022").split(";")))
+  sudo_users = list(set(x for x in environ.get("SUDO_USERS", "1649649855").split(";")))
   try:
     from_chats = list(set(int(x) for x in environ.get("FROM_CHATS").split()))
     to_chats = list(set(int(x) for x in environ.get("TO_CHATS").split()))
