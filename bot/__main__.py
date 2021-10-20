3d8964baeb792bc0422becc7413fc2a82c45cc75
susi1403 <92388675+susi1403@users.noreply.github.com>
2021-10-20 23:26:07 +0200

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -10,17 +10,34 @@ from bot.helper.utils import get_formatted_chat
 def work(client, message):
     caption = None
     msg = None
-    try:
-      for chat in to_chats:
-        #unklar
-        if caption:
-          message.copy(chat, caption=caption)
-        elif msg:
-          app.send_message(chat, msg)
-        else:
-          message.copy(chat)
-    except Exception as e:
-      LOG.error(e)
+    if remove_strings:
+      for string in remove_strings:
+        if message.media and not message.poll:
+          caption = message.caption.html.replace(string, replace_string)
+        elif message.text:
+          msg = message.text.html.replace(string, replace_string)
+    if advance_config:
+      try:
+        for chat in chats_data[message.chat.id]:
+          if caption:
+            message.copy(chat, caption=caption)
+          elif msg:
+            app.send_message(chat, msg, parse_mode="html")
+          else:
+            message.copy(chat)
+      except Exception as e:
+        LOG.error(e)
+    else:
+      try:
+        for chat in to_chats:
+          if caption:
+            message.copy(chat, caption=caption)
+          elif msg:
+            app.send_message(chat, msg)
+          else:
+            message.copy(chat)
+      except Exception as e:
+        LOG.error(e)
 
 #/forward command
 @app.on_message(filters.user(sudo_users) & filters.command(["fwd", "forward"]), group=1)
